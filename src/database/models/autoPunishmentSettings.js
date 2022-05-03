5fb331e122a861a234f4e459007c4b71747f0b75
Zyla <erencana12@gmail.com>
2022-05-03 14:12:09 +0300

    Removed auto punishment system

--- src/database/models/autoPunishmentSettings.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose'
-
-const AutoPunishmentSettings = new mongoose.Schema({
-  warns: Number,
-  punishment: String,
-  duration: String,
-})
-
-export default mongoose.model('auto-punishment-settings', AutoPunishmentSettings)
